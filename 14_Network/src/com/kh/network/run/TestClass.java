5010f949ff10299f7692bf3f9a01a58673ae8fe5
cjysy0104 <cjysy0104@gmail.com>
2025-08-21 11:44:48 +0900

    create_howToGit

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -5,9 +5,12 @@ public class TestClass {
 	public static void main(String[] args) {
 
 		
-		// Git Test PUSH
-		// Home Sweet Home
-		// happy
+		// Git
+		// 1. 본인의 workspace - gitBash 실행
+		// 2. git pull origin master : 저장소에서 끌어오기
+		// 3. git add . : 작업 후 add
+		// 4. git commit -m "작업내용" : commit 로컬에 저장완료
+		// 5. git push origin master : 저장소에 보내기
 	}
 
 }
