4107ee5a72d5f223ebb4e3b1b4bf17b54c92761c
cjysy0104 <cjysy0104@gmail.com>
2025-08-22 10:46:38 +0900

    Create MusicPJ

---
+++ Music_Practice/src/com/kh/practice/list/music/controller/MusicController.java
@@ -0,0 +1,43 @@
+package com.kh.practice.list.music.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.practice.list.music.model.vo.Music;
+
+public class MusicController {
+
+	private List list = new ArrayList();
+	
+	public int addList(Music music) {
+		return 0;
+	}
+	
+	public int addAtZero(Music music) {
+		return 0;
+	}
+	
+	public List printAll() {
+		return null;
+	}
+	
+	public Music searchMusic(String title) {
+		return null;
+	}
+	
+	public Music removeMusic(String title) {
+		return null;
+	}
+	
+	public Music setMusic(String title, Music music) {
+		return null;
+	}
+	
+	public int ascTitle() {
+		return 0;
+	}
+	
+	public int descSinger() {
+		return 0;
+	}
+}
